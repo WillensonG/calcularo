e87351170fa85c0b0b51c17a661a599a8587ed2b
Wex_2 <willensonr.g@gmail.com>
2023-07-24 23:13:24 -0400

    Agregar archivos de proyecto.

---
+++ calcularo/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace calcularo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 97);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button9
+            // 
+            this.button9.Image = global::calcularo.Properties.Resources.icons8_slash_67;
+            this.button9.Location = new System.Drawing.Point(287, 336);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(115, 48);
+            this.button9.TabIndex = 3;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Image = global::calcularo.Properties.Resources.icons8_two_67;
+            this.button16.Location = new System.Drawing.Point(94, 174);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(58, 48);
+            this.button16.TabIndex = 2;
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Image = global::calcularo.Properties.Resources.icons8_three_67;
+            this.button15.Location = new System.Drawing.Point(178, 174);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(58, 48);
+            this.button15.TabIndex = 2;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Image = global::calcularo.Properties.Resources.icons8_5_50;
+            this.button12.Location = new System.Drawing.Point(94, 247);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(58, 48);
+            this.button12.TabIndex = 2;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Image = global::calcularo.Properties.Resources.icons8_multiplication_50;
+            this.button11.Location = new System.Drawing.Point(287, 282);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(115, 48);
+            this.button11.TabIndex = 2;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Image = global::calcularo.Properties.Resources.icons8_eight_50;
+            this.button8.Location = new System.Drawing.Point(94, 312);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(58, 48);
+            this.button8.TabIndex = 2;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Image = global::calcularo.Properties.Resources.icons8_subtraction_60;
+            this.button7.Location = new System.Drawing.Point(287, 228);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(115, 48);
+            this.button7.TabIndex = 2;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::calcularo.Properties.Resources.icons8_seven_50;
+            this.button5.Location = new System.Drawing.Point(12, 312);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(58, 48);
+            this.button5.TabIndex = 2;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::calcularo.Properties.Resources.icons8_nine_50;
+            this.button4.Location = new System.Drawing.Point(178, 312);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 48);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::calcularo.Properties.Resources.icons8_sum_60;
+            this.button6.Location = new System.Drawing.Point(287, 174);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 48);
+            this.button6.TabIndex = 2;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::calcularo.Properties.Resources.icons8_four_50;
+            this.button3.Location = new System.Drawing.Point(12, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 48);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::calcularo.Properties.Resources.icons8_six_50;
+            this.button2.Location = new System.Drawing.Point(178, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 48);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::calcularo.Properties.Resources.icons8_one_50__2_;
+            this.button1.Location = new System.Drawing.Point(12, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
